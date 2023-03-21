73371349b89eb87dc26721862963bef9f6031cde
Prashanna Rajbhandari <prashanna.rajbhandari1@gmail.com>
2023-03-22 02:22:00 +0530

    feat: CoreData using FetchRequest

--- SwiftUI-CoreData/ContentView.swift
+++ SwiftUI-CoreData/ContentView.swift
@@ -5,71 +5,90 @@
 //  Created by Prashanna Rajbhandari on 22/03/2023.
 //
 
-import SwiftUI
 import CoreData
+import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
+    @State var TextFieldText: String = ""
+
     @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+        entity: FruitEntity.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \FruitEntity.name, ascending: true)]
+    )
+    var fruits: FetchedResults<FruitEntity>
 
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+            VStack(spacing: 20) {
+                TextField("Add Fruit Here", text: $TextFieldText)
+                    .padding()
+
+                Button {
+                    addItem()
+                } label: {
+                    Text("Add Fruit")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(.blue)
+                        .cornerRadius(10)
+                }
+
+                List {
+                    ForEach(fruits) { fruit in
+                        Text(fruit.name ?? "")
+                            .onTapGesture {
+                                updateItems(fruit: fruit)
+                            }
                     }
-                }
-                .onDelete(perform: deleteItems)
+                    .onDelete(perform: deleteItems)
+                }.listStyle(PlainListStyle())
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
+
+            .navigationTitle("Fruits")
             Text("Select an item")
         }
     }
 
     private func addItem() {
         withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            let newFruit = FruitEntity(context: viewContext)
+            newFruit.name = TextFieldText
+            saveItems()
+            TextFieldText = ""
+        }
+    }
+    
+    private func updateItems(fruit:FruitEntity){
+        withAnimation{
+            let currName = fruit.name ?? ""
+            let newName =  currName + "!"
+            fruit.name = newName
+            
+            
+            saveItems()
         }
     }
 
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
+            guard let index = offsets.first else { return }
+            let fruitEntity = fruits[index]
+            viewContext.delete(fruitEntity)
 
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            saveItems()
+        }
+    }
+
+    private func saveItems() {
+        do {
+            try viewContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
     }
 }
